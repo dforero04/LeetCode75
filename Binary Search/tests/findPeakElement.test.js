0693832b277e05f1c3c9b832d64e41b7373fd192
dforero04 <dwforero4@gmail.com>
2024-02-09 16:06:31 -0600

    Complete findPeakElement and test. The solution needs further review and documentation

---
+++ Binary Search/tests/findPeakElement.test.js
@@ -0,0 +1,8 @@
+const findPeakElement = require("../162_find_peak_element");
+
+it.each([
+  [2, [1, 2, 3, 1]],
+  [5, [1, 2, 1, 3, 5, 5, 4]],
+])("should return %s for input array = %s", (result, array) => {
+  expect(findPeakElement(array)).toEqual(result);
+});
