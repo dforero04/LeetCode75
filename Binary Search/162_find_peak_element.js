e1cb66fa8a255f69d026bfddb56c28d559c8da35
dforero04 <dwforero4@gmail.com>
2024-02-12 10:22:48 -0600

    Finish documentation for findPeakElement and adjust algorithm

--- Binary Search/162_find_peak_element.js
+++ Binary Search/162_find_peak_element.js
@@ -13,13 +13,31 @@ function findPeakElement(nums) {
     right = nums.length - 1,
     mid;
 
-  while (left < right) {
-    mid = Math.floor((right + left) / 2);
-    if (nums[mid] > nums[mid + 1]) right = mid;
-    else left = mid + 1;
-  }
+  while (left <= right) {
+    mid = Math.floor(left + (right - left) / 2);
 
-  return left;
+    // Check left neighbor; if neighbor is greater, search left side
+    if (mid > 0 && nums[mid] < nums[mid - 1]) right = mid - 1;
+    // Check right neighbor; if neighbor is greater, search right side
+    else if (mid < nums.length - 1 && nums[mid] < nums[mid + 1]) left = mid + 1;
+    // Checked both and nums[mid] is greater than both
+    else return mid;
+  }
 }
+
+/*
+Completed with help. A key aspect of this problem was hidden in the constraints section, where nums[i] will never equal nums[i+1]. This is an important note for coming up with the appropriate solution.
+
+This algorithm is also majorly based on Binary Search, but tweaked a little since the array is not sorted. Instead, we just binary search the side that is greater than nums[mid].
+
+Algorithm:
+- Having 2 pointers, left and right, we find the midpoint.
+-- About the midpoint, in order for it not to potentially "overflow" the array, use the formula left + ((right - left) / 2)
+- Check if midpoint is less than value directly to the left, AND check if midpoint is not at index 0
+-- We check if index is not at 0 because it will solve the edge case of the peak being at index 0
+- Check if midpoint is less than value directly to the right, AND check if midpoint is not at index nums.length - 1
+-- Similar to above, we check if index is not at nums.length - 1 because it will solve the edge case of the peak being at the end of the array.
+- Return mid if neither if is successful
+*/
 
 module.exports = findPeakElement;
